f6c284fece3aadb2d32fc6390b53a8877db5c90a
Will Ponczak <willponczak@gmail.com>
2025-06-05 17:47:02 -0400

    Method outlines for track_builder/main.py. Month filtering for 2019 ASTD data (first and last day of every month). Still need to group all of the days together into one df.

--- track_builder/main.py
+++ track_builder/main.py
@@ -1,3 +1,47 @@
 def hello_world():
     return "Hello, World!"
 
+
+def build_track_table(dataset):
+    """
+    This will be the main function users call when building the map.
+    The purpose of this function is to build a dictionary table with
+    the keys being ["month", "segment_id", "track_id"]. The "track_id"
+    key is just a placeholder for the track.
+
+    Args:
+
+    Returns:
+
+    """
+
+
+def compare_multi_month_ids(shipid, date_time_utc, flagname, ice_class, astd_cat, sizegorup_gt,
+                          fuel_quality, dist_nextpoint, dist_secpoint, longitude, latitude):
+    """
+    This method will compare ship id's across multiple months
+    to find all of the different ship id's for one ship.
+
+    Args:
+
+    Returns:
+    """
+
+    #Compare dist_nextpoint
+
+def compare_dist_nextpoint(thisShipID, otherShipIDList):
+    """
+    This is a helper method to compare the dist_nextpoint column
+    between different shipid's within the same month.
+    """
+
+    # given a single shipid, we need to find its match for 
+    # the next month (a list of ship ids)
+
+    
+
+
+
+
+
+
